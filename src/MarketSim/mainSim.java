8475a729df0467f7df4ea4eaf667621e4b62ce14
imthedion <laudigan2014@gmail.com>
2020-05-17 22:21:25 +0100

    for loop

--- src/MarketSim/mainSim.java
+++ src/MarketSim/mainSim.java
@@ -1,6 +1,7 @@
 package MarketSim;
 
 public class mainSim {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -9,13 +10,21 @@ public class mainSim {
 		 
 		 i.setName("HSE");
 		 i.setInvestorID(9283);
+		 
 		 c.setCoName("Green Cross");
 		 c.setCompanyID(41234);
 		 
 		 i.printInv();
 		 c.printCompany();
+		 
+		 System.out.println("hello world");
+		 
+		 for(int counter = 1; counter <= 100; counter++) {
+			 System.out.println("hello mawe" + counter);
+			 
+		 }
 
 
 	}
-
+	
 }
